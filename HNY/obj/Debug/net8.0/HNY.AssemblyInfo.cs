abf13740155955626004a05d489b3dfd353d54bd
Nerf-Chamber <lzpobletpobletos@gmail.com>
2026-01-01 12:52:16 +0100

    Height adaptable to buffer build

--- HNY/obj/Debug/net8.0/HNY.AssemblyInfo.cs
+++ HNY/obj/Debug/net8.0/HNY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d248892e5220b0dc8bccf725d23511865f05ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf08e7c89d3f498ed281ae32c88dccdbd8997f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
